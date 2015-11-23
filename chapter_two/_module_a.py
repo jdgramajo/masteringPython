15d07f156e8af61ba8325f03228365381e1e614f
Juan Diego Gramajo <juan.gramajo@xoom.com>
2015-11-23 17:58:07 -0600

    Gets package data files.

--- chapter_two/_module_a.py
+++ chapter_two/_module_a.py
@@ -1,2 +1,7 @@
+from pkgutil import get_data
+
 def a_func():
     print("Function in module A")
+
+def a_extract_config():
+    return get_data(__package__, "data/config_file.txt").decode("utf-8")
--- chapter_two/module_b.py
+++ chapter_two/module_b.py
@@ -3,5 +3,8 @@ from . import _module_a
 def b_func():
     print("Function in module B")
 
-def a_func_from_b():
+def a_func():
     _module_a.a_func()
+
+def get_package_config():
+    return _module_a.a_extract_config()
