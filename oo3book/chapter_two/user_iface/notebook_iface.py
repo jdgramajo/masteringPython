004fcd54ee426f0ddf7d6eb00e4cb2b79d6be02f
Juan Diego Gramajo <juan.gramajo@xoom.com>
2015-12-29 12:36:45 -0600

    Finishes the iface for the notebook practice using cmd. No validations, but good practice.

--- oo3book/chapter_two/user_iface/notebook_iface.py
+++ oo3book/chapter_two/user_iface/notebook_iface.py
@@ -29,16 +29,35 @@ class NotebookIface(cmd.Cmd):
         if self.notebook == None:
             print('No notebook created yet, create one first.')
         else:
+            # These next three lines really really are bad, but I'm just
+            # learning so I'll leave them as they are 'cause they work.
             all_args = ''.join(args)
-            self.notebook.new_note(all_args.split()[0:-1], 
-                all_args.split()[-1::].split(':')[1])
+            self.notebook.new_note(' '.join(all_args.split()[0:-1]), 
+                all_args.split()[-1::][0].split(':')[1])
 
     def do_show_all(self, args):
         'Shows all memos and tags'
-        print(len(self.notebook.notes))
         for note in self.notebook.notes:
             print('id: {}\nMemo: {}\ntag: {}\n'.format(note.id,
                 note.memo, note.tags))
+
+    def do_mod_memo(self, args):
+        """\
+        Modifies note's memo.
+        Usage: 'mod_memo id new memo content'
+        NOTE: There is no validation, enter a valid id!
+        """
+        all_args = ''.join(args)
+        self.notebook.modify_memo(int(all_args[0]), ''.join(all_args[1::]))
+
+    def do_mod_tag(self, args):
+        """\
+        Modifies note's tag.
+        Usage: 'mod_memo id new tag'
+        NOTE: There is no validation, enter a valid id!
+        """
+        all_args = ''.join(args)
+        self.notebook.modify_tags(int(all_args[0]), ''.join(all_args[1::]))
 
     def do_reset(self, args):
         'Clear screen.'
